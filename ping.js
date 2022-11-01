37e9a0c73219c3e530d2a4b6c688931bb5d1edb3
Pedro Martins Justi <108304346+paulistaLH@users.noreply.github.com>
2022-11-01 17:29:18 -0300

    Update ping.js

--- ping.js
+++ ping.js
@@ -4,3 +4,10 @@ let botaoJogarEl = document.querySelector("#jogar");
 botaoJogarEl.addEventListener('click', () =>{
     opcoesEl.classList.remove("sumir");
 });
+
+let configurarEl = document.querySelector("#configurar");
+let menuEl = document.querySelector("#menu");
+
+configurarEl.addEventListener('click', () =>{
+    menuEl.classList.add("sumir");
+});
