0da8237bb469a0f5397f19a12affe584320ebbbc
VictorEduReis <104006964+VictorEduReis@users.noreply.github.com>
2022-11-01 17:42:14 -0300

    Update ping.js

--- ping.js
+++ ping.js
@@ -11,3 +11,8 @@ let menuEl = document.querySelector("#menu");
 configurarEl.addEventListener('click', () =>{
     menuEl.classList.add("sumir");
 });
+
+let partidaEL = document.querySelector("#partida");
+partidaEL.addEventListener('click', () =>{
+    menuEl.classList.add("sumir");
+});
